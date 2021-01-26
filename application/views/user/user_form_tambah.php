64a6b63bd7dfcbc1439f3c6ed21d1bd5a1a16ba0
Honey-bee-code <hanifinida@gmail.com>
2021-01-27 01:06:08 +0800

    gak jadi dah, males

--- application/views/user/user_form_tambah.php
+++ application/views/user/user_form_tambah.php
@@ -1,6 +1,4 @@
 <!-- Content Header (Page header) -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.7/cropper.min.css" crossorigin="anonymous"/>
-
 
 <section class="content-header">
   <h1>
@@ -65,17 +63,13 @@
 
                         <div class="form-group">
                             <label>Photo</label>
-                            <br>
-                            <img class="card-img-top img-fluid " onclick="$('#photo').click()" id="img" 
-                                src="<?=site_url('assets/images/picture.jpg')?>">
-                            <br>
                             <small>(Biarkan kosong jika tidak ada gambar)</small>
-                            <input name="photo"  id="photo" type="file" accept="image/*" class="form-control">
+                            <input name="photo" type="file" accept="image/*" class="form-control">
                         </div>
                         
                         
                         <div class="form-group">
-                            <button type="submit" class="btn btn-success btn-sm"><i class="fa fa-send"></i> Simpan</button>
+                            <button type="submit" id="submit" class="btn btn-success btn-sm"><i class="fa fa-send"></i> Simpan</button>
                             <button type="reset" class="btn btn-sm"><i class="fa fa-refresh"></i> Reset</button>
                         </div>
 
@@ -85,84 +79,4 @@
         </div>
     </div>
 </section>
-    <div class="modal fade" id="modalcrop">
-        <div class="modal-dialog modal-md" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title"></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div>
-                <img id='modalimg' class='img-fluid' src="" />
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type='button' class='btn btn-danger btn-sm' onclick='cropping()'>Crop Gambar</button>
-            </div>
-            </div>
-        </div>
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.7/cropper.min.js" crossorigin="anonymous"></script>
-    <script>
-    //Global Variabel (Optional)
-    var croppers=null;
-    var file=null;
-
-    //Trigger Ketika Input Type File di Click
-    $('#photo').change(function () {
-        fileCount = this.files.length;
-        if(fileCount && (this.files[0].type=="image/jpeg"||this.files[0].type=="image/jpg"||this.files[0].type=="image/png")){
-            //Menampilkan Gambar ke Modal
-            $('#modalimg').attr("src",URL.createObjectURL(event.target.files[0]));
-
-            //Menampilkan Popup Modal
-            $('#modalcrop').modal('show');
-
-            //Memberikan Timeout/Waktu Jeda selama 0.5 Detik Untuk Menyiapkan Cropper JS
-            setTimeout(function(){
-                const image = document.getElementById('modalimg');
-                var height = $('#img').height();
-                var width = $('#img').width();
-
-                //Parameter dari Cropper JS (Baca di https://fengyuanchen.github.io/cropperjs/)
-                var cropper = new Cropper(image, {
-                  viewMode: 3,
-                  aspectRatio: 4 / 4,
-                  movable: false,
-                  zoomable: false,
-                  width:width,
-                  height:height,
-                  crop(event) {
-                  },
-                });
-
-                //Assignment hasil cropper ke variabel Global Croppers
-                croppers=cropper;
-            },500);
-        }else{alert('Ekstensi File Harus Image');}
-      });
-
-      //Fungsi Ketika Tombol Crop Gambar di Jalankan
-      function cropping(){
-        croppers.getCroppedCanvas({height:160,width:160}).toBlob(function(blob){
-          //Untuk mengganti tampilan dengan gambar yang sudah di crop
-            $("#img").attr('src',''+URL.createObjectURL(blob));
-            // $("#photo").attr('data',''+URL.createObjectURL(blob));
-
-          //menyimpan hasil gambar yang sudah di potong ke dalam variabel global file
-
-          //Menutup Popup Modal
-          $('#modalcrop').modal('hide');
-        },'image/jpeg',0.8);
-        
-    }
-
-    </script>
-
+   
