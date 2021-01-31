2658f125bce7ce3a21b83a50bd8240d347396f8e
Honey-bee-code <hanifinida@gmail.com>
2021-02-01 01:03:18 +0800

    add time script

--- application/views/transaksi/penjualan/penjualan_form.php
+++ application/views/transaksi/penjualan/penjualan_form.php
@@ -107,6 +107,9 @@
                                     </button>
                                 </div>
                             </td>
+                            <td colspan="2" style="text-align: center">
+                               <small>Input time : </small><b><span id="jamServer"><?=date("H:i:s");?></span></b>
+                            </td>
                         </tr>
                     </table>
                 </div>
@@ -225,7 +228,7 @@
                     <table width="100%">
                         <tr>
                             <td style="vertical-align:top">
-                                <label for="catatan">Nota</label>
+                                <label for="catatan">Catatan</label>
                             </td>
                             <td>
                                 <div>
@@ -609,4 +612,42 @@ $(document).on('click', '#cancel_payment', function() {
 })
 
 
+</script>
+
+<script>
+    var serverClock = jQuery("#jamServer");
+if (serverClock.length > 0) {
+    showServerTime(serverClock, serverClock.text());
+}
+ 
+function showServerTime(obj, time) {
+    var parts   = time.split(":"),
+        newTime = new Date();
+
+    newTime.setHours(parseInt(parts[0], 10));
+    newTime.setMinutes(parseInt(parts[1], 10));
+    newTime.setSeconds(parseInt(parts[2], 10));
+
+    var timeDifference  = new Date().getTime() - newTime.getTime();
+
+    var methods = { 
+        displayTime: function () {
+            var now = new Date(new Date().getTime() - timeDifference);
+            obj.text([
+                methods.leadZeros(now.getHours(), 2),
+                methods.leadZeros(now.getMinutes(), 2),
+                methods.leadZeros(now.getSeconds(), 2)
+            ].join(":"));
+            setTimeout(methods.displayTime, 500);
+        },
+ 
+        leadZeros: function (time, width) {
+            while (String(time).length < width) {
+                time = "0" + time;
+            }
+            return time;
+        }
+    }
+    methods.displayTime();
+}
 </script>
