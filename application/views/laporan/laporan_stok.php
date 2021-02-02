d2382ec4f91c38382b6f988b3d1a97d5f24ee0bd
Honey-bee-code <hanifinida@gmail.com>
2021-02-02 08:32:29 +0700

    change modal title at stok report

--- application/views/laporan/laporan_stok.php
+++ application/views/laporan/laporan_stok.php
@@ -74,7 +74,7 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-                <h4 class="modal-title">Detail Data Stok</h4>
+                 <h4 class="modal-title">Detail Data Stok</h4>
             </div>
             <div class="modal-body table-responsive">
                 <table class="table table-bordered no-margin">
