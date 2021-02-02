778bfed377c78420ce5f9d2cd08376fbaf70853e
Honey-bee-code <hanifinida@gmail.com>
2021-02-02 22:01:23 +0700

    delete webcam

--- application/controllers/Penjualan.php
+++ application/controllers/Penjualan.php
@@ -131,10 +131,5 @@ class Penjualan extends CI_Controller {
         }
         echo "<script>window.location='" .site_url('laporan/penjualan'). "'</script>";
 	}
-	
-	public function scan()
-	{
-		$this->template->load('template', 'transaksi/penjualan/code_scan' );
-	}
 
 }
